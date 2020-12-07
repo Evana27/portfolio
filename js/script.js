c4a559855637e2a5211c36c5866988094ebb29fb
Evana27 <71651380+Evana27@users.noreply.github.com>
2020-12-07 22:09:27 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+    //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+    //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+    
+    });
+    
+      // banner part code 
+      $('.owl-carousel').owlCarousel({
+        center: true,
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout: 2000,
+        smartSpeed: 900,
+    }); 
